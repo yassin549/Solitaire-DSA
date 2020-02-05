1880865150237f2546b08a40a8d9a1ecab8fb421
yassin549 <khoualdiyassin26@gmail.com>
2020-02-05 12:25:00 +0500

    Optimized event handling for responsive input

---
+++ queue.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear = None
+
+    def isEmpty(self):
+        return self.front is None
+
+    def enqueue(self, data):
+        new_node = Node(data)
+        if self.is_empty():
+            self.front = new_node
+            self.rear = new_node
+        else:
+            self.rear.next = new_node
+            self.rear = new_node
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        temp = self.front
+        self.front = self.front.next
+        if self.front is None:
+            self.rear = None
+        return temp.data
+
+    def top(self):
+        return self.front.data if self.front else None
+
+    def displayQueue(self):
+        current = self.front
+        while current:
+            print(current.data)
+            current = current.next
+
+# jzn jn
